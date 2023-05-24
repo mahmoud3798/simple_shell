4f8ec80bf2abebcc2131748c6de84db3753a87b9
mahmoud <mahmoud.m3798@gmail.com>
2023-05-24 12:59:17 -0700

    update

--- shell.h
+++ shell.h
@@ -1,16 +1,57 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <signal.h>
 
-#define BUFFER_SIZE 1024
+/**
+ * struct list - path directory list structure.
+ * @dir: directory path.
+ * @next: pointer to next directory node.
+ */
+typedef struct list
+{
+	char *dir;
+	struct list *next;
+} list_t;
+int start_shell(list_t *path, char **env, char *program_name, int mode);
+int execute_buffer(char *buffer, list_t *path, char **env, char *program_name);
+int execute_command(char *new_buffer, list_t *path, char **env,
+			int final, char *program_name);
+int execute_fork(char **input, char *program_name);
+list_t *list_path(char **env);
+list_t *create_list(char **environ);
+list_t *add_list(list_t **head, char *dir);
+void free_list(list_t *head);
+char *get_path(char *buffer, list_t **path);
+char *aux_get_path(list_t *list_pointer,
+	char *slash_command, char *slash_input, char *input);
+char *clean_spaces(char *buffer);
+char **create_argv(char *input_buffer, list_t **path);
+void free_argv(char **argv);
+char *clean_comments(char *buffer);
+int check_builtin(char *command);
+int check_syntax(char *buffer);
+int builtins(char **input, char **env);
+void print_help(char **input);
+int str_len(char *s);
+char *str_cpy(char *dest, char *src);
+char *str_dup(char *str);
+char *str_cat(char *dest, char *src);
+char *str_con(char *s1, char *s2);
+int not_empty(char *input_buffer);
+int str_twins(char *s1, char *s2);
+int str_count(char *buffer, char c);
+char *str_tr(char *buffer, char old_char, char new_char);
+void ctrl_c(__attribute__((unused)) int x);
+void print_error(char *program_name, char *input, int error_num);
 
-ssize_t read_command(char **buffer, size_t *bufsize);
-int parse_command(char *buffer, char **args);
-void execute_command(char **args);
-
-#endif /* SHELL_H */
+#endif
