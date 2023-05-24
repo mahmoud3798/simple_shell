a627114cef35006a49d14e6f8cb2199cabbd74ec
mahmoud <mahmoud.m3798@gmail.com>
2023-05-24 12:09:35 -0700

    update

---
+++ main.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * read_command - reads a command from stdin
+ * @buffer: pointer to buffer to store command
+ * @bufsize: size of buffer
+ * Return: number of characters read, or -1 on failure
+ **/
+ssize_t read_command(char **buffer, size_t *bufsize)
+{
+	printf("$ ");
+	return (getline(buffer, bufsize, stdin));
+}
+
+/**
+ * parse_command - parses a command into arguments
+ * @buffer: command to parse
+ * @args: pointer to array to store arguments
+ * Return: number of arguments
+ **/
+int parse_command(char *buffer, char **args)
+{
+	char *token;
+	int i = 0;
+
+	token = strtok(buffer, " \n");
+	while (token != NULL)
+	{
+		args[i++] = token;
+		token = strtok(NULL, " \n");
+	}
+	args[i] = NULL;
+	return (i);
+}
+
+/**
+ * execute_command - executes a command in a child process
+ * @args: arguments for command
+ **/
+void execute_command(char **args)
+{
+	execvp(args[0], args);
+	printf("%s: command not found\n", args[0]);
+	exit(1);
+}
+/**
+* main - Entry point
+* Return: always 0
+*/
+int main(void)
+{
+	char *buffer;
+	size_t bufsize = BUFFER_SIZE;
+	pid_t pid;
+	int status;
+
+	buffer = (char *)malloc(bufsize * sizeof(char));
+	if (buffer == NULL)
+	{
+		perror("Unable to allocate buffer");
+		exit(1);
+	}
+	while (1)
+	{
+		ssize_t nread = read_command(&buffer, &bufsize);
+
+		if (feof(stdin))
+		{
+			printf("\n");
+			exit(0);
+		}
+		if (nread > 1)
+		{
+			/* Parse the command and arguments*/
+			char *args[10];
+			int argc = parse_command(buffer, args);
+
+			/*Fork a child process and execute the command*/
+			pid = fork();
+			if (pid == 0)
+			{
+				execute_command(args);
+			}
+			else
+			{
+				waitpid(pid, &status, 0);
+			}
+		}
+	}
+	free(buffer);
+	return (0);
+}
